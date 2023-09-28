4a844ae06e781d32707e206f52bd38024f7e78a9
smith <smithbabawande@gmail.com>
2023-09-28 17:37:29 +0100

    Completed Assignment

---
+++ lib/app.py
@@ -0,0 +1,6 @@
+
+my_lc = [n**2 for n in range(10)]
+print(my_lc)
+
+kc = [range(1,10)]
+print(kc)
--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+ 
+    even_list = [item for item in num_list if item % 2 == 0]
+    return even_list
+
 
 def make_exclamation(sentence_list):
-    pass
+    new_list = [n + "!" for n in sentence_list]
+    return new_list
